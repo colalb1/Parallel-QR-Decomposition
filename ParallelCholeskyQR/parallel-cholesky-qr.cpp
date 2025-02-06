1a3eb683600e8c9b4e0d0aff91ee49e507b65f7f
Colin <colin.albs@gmail.com>
2025-02-05 21:21:15 -0600

    Add parallel Cholesky QR file.

---
+++ ParallelCholeskyQR/parallel-cholesky-qr.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+typedef Eigen::MatrixXd Matrix;
+
+// Parallel Cholesky QR decomposition
+std::pair<Matrix, Matrix> parallel_cholesky_QR_decomposition(const Matrix &A)
+{
+}
