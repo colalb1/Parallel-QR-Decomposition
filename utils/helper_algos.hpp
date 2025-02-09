e68abf129313052550e44f27ecd8c44423c09412
colalb1 <Colin.albs@gmail.com>
2025-02-09 16:54:03 -0600

    Added sCQR3.

--- utils/helper_algos.hpp
+++ utils/helper_algos.hpp
@@ -223,3 +223,18 @@ std::pair<Matrix, Matrix> parallel_shifted_cholesky_QR(const Matrix &A)
 
     return {Q, R};
 }
+
+// Shifted Cholesky QR decomposition 3
+std::pair<Matrix, Matrix> shifted_cholesky_QR_3(const Matrix &A)
+{
+    // Initial shifted extraction (shift for stability)
+    auto [Q_1, R_1] = parallel_shifted_cholesky_QR(A);
+
+    // Second Q and R extraction
+    auto [Q, R_2] = cholesky_QR_2(Q_1);
+
+    // Calculate final R
+    Matrix R = R_2 * R_1;
+
+    return {Q, R};
+}
