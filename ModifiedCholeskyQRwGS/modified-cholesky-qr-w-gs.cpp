9d0d9366bf175c7ebf83ee47ebbe3d2e8436126d
Colin <34983726+colalb1@users.noreply.github.com>
2025-02-15 15:35:59 -0600

    Init modified Cholesky QR w GS. Write tomorrow if time allows.

---
+++ ModifiedCholeskyQRwGS/modified-cholesky-qr-w-gs.cpp
@@ -0,0 +1,13 @@
+#include <utils/helper_algos.hpp>
+
+std::pair<Matrix, Matrix> modified_cholesky_QR_w_gram_schmidt(const Matrix &A)
+{
+    int const m = A.rows();
+    int const n = A.cols();
+    int const block_size = std::min(64, n);          // Adjust block size dynamically
+    int const k = (n + block_size - 1) / block_size; // Number of panels
+    int const num_threads = omp_get_max_threads();
+
+    Matrix Q = Matrix::Zero(m, n);
+    Matrix R = Matrix::Zero(n, n);
+}
