71e97ee6546353ba88ea24b8dffe24a3dbb7a0cc
Colin <colin.albs@gmail.com>
2025-02-23 20:32:28 -0600

    Code removal and update.

--- CholeskyQR/cholesky-qr.cpp
+++ CholeskyQR/cholesky-qr.cpp
@@ -5,7 +5,7 @@
 typedef Eigen::MatrixXd Matrix;
 
 // Cholesky QR decomposition
-std::pair<Matrix, Matrix> cholesky_QR(const Matrix &A)
+std::pair<Matrix, Matrix> cholesky_QR(Matrix &A)
 {
     // Compute Gram matrix
     Eigen::LLT<Matrix> cholesky_factorization(A.transpose() * A);
@@ -17,34 +17,4 @@ std::pair<Matrix, Matrix> cholesky_QR(const Matrix &A)
     Matrix Q = A * R.inverse();
 
     return {Q, R};
-}
-
-int main()
-{
-    // Tall matrix dimensions
-    int num_rows = 100000;
-    int num_cols = 100;
-    int num_iterations = 200;
-
-    // Timing for serial decomposition
-    double total_time_serial = 0.0;
-    for (int i = 0; i < num_iterations; ++i)
-    {
-        std::cout << "Iteration: " << i << " \n";
-
-        // Generate a random tall matrix A
-        Matrix A = Matrix::Random(num_rows, num_cols);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        auto [Q, R] = cholesky_QR(A);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> elapsed = end - start;
-
-        total_time_serial += elapsed.count();
-    }
-    double average_time_serial = total_time_serial / num_iterations;
-
-    // Output results
-    std::cout << "Average time for cholesky_QR_decomposition: " << average_time_serial << " seconds\n";
 }
