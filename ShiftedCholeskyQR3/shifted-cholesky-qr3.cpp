950bb6ed2ed8575af42d7f059dc97b21daada44b
Colin <colin.albs@gmail.com>
2025-02-23 20:37:32 -0600

    Remove tests.

--- ShiftedCholeskyQR3/shifted-cholesky-qr3.cpp
+++ ShiftedCholeskyQR3/shifted-cholesky-qr3.cpp
@@ -1,7 +1,7 @@
 #include <utils/helper_algos.hpp>
 
 // Shifted Cholesky QR decomposition 3
-std::pair<Matrix, Matrix> shifted_cholesky_QR_3(const Matrix &A)
+std::pair<Matrix, Matrix> shifted_cholesky_QR_3(Matrix &A)
 {
     // Initial shifted extraction (shift for stability)
     auto [Q_1, R_1] = parallel_shifted_cholesky_QR(A);
@@ -13,30 +13,4 @@ std::pair<Matrix, Matrix> shifted_cholesky_QR_3(const Matrix &A)
     Matrix R = R_2 * R_1;
 
     return {Q, R};
-}
-
-int main()
-{
-    // Tall matrix dimensions
-    int num_rows = 100000;
-    int num_cols = 100;
-
-    // Timing
-    double total_time = 0.0;
-
-    // Generate a random tall matrix A
-    Matrix A = Matrix::Random(num_rows, num_cols);
-
-    auto start = std::chrono::high_resolution_clock::now();
-    auto [Q, R] = shifted_cholesky_QR_3(A);
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> elapsed = end - start;
-
-    total_time += elapsed.count();
-
-    // Output results
-    std::cout << "Time for shifted_cholesky_QR_3: " << total_time << " seconds\n";
-
-    return 0;
 }
